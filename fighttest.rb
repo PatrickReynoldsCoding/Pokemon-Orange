7489321d6eb7aa67b58973000d1eaa5e9c355c3b
PatrickReynoldsCoding <99245456+PatrickReynoldsCoding@users.noreply.github.com>
2022-04-18 15:09:04 +0100

    Update fighttest.rb

--- fighttest.rb
+++ fighttest.rb
@@ -8,7 +8,7 @@ class Pokedex
   end
 
   def get_stats_name(name)
-    @pokemon_data.find {|row| row[0] == name }
+    @pokemon_data.find {|row| row[1] == name }
   end
 
   def get_stats_id(name)
@@ -17,14 +17,52 @@ class Pokedex
 end
 
 
-pokedex = Pokedex.new('csv/Kanto Pokemon Spreadsheet.csv')
+$pokedex = Pokedex.new('csv/Kanto Pokemon Spreadsheet.csv')
 randEncount = Pokedex.new('csv/Kanto Pokemon Spreadsheet.csv')
 
+puts "pick your pokemon!"
+$choice1 = gets.chomp
+
+puts "you picked #{$pokedex.get_stats_name($choice1)[1]}(HP:#{$pokedex.get_stats_name($choice1)[4]})(Atk:#{$pokedex.get_stats_name($choice1)[5]})!" 
+
+partner1 = $pokedex.get_stats_name($choice1)
 
 randPoke = (0 + rand(151)).to_s
 
+puts "Oh no!"
+puts "a wild #{$pokedex.get_stats_id(randPoke)[1]}(HP:#{$pokedex.get_stats_id(randPoke)[4]})(Atk:#{$pokedex.get_stats_id(randPoke)[5]}) appeared!"
 
-puts pokedex.get_stats_name(randPoke)[2]
+#pat1Atk = $pokedex.get_stats_name($choice1)[5] /3
+
+
+def moveList(x,y)
+  puts ""
+  puts "Pick a move"
+  puts "1. #{x.get_stats_name(y)[10]}" 
+  puts "2. #{x.get_stats_name(y)[11]}" 
+  puts "3. #{x.get_stats_name(y)[12]}" 
+  puts "4. #{x.get_stats_name(y)[13]}" 
+end
+
+
+
+def newRound
+  puts "1. Attack"
+  puts "2. Pokemon"
+  puts "3. Bag"
+  puts "4. Run"
+  roundInput = gets.chomp
+
+  if roundInput == "1"
+    then puts moveList($pokedex, $choice1)
+    else 
+      puts "error"
+  end
+
+end
+
+puts newRound
+
 
 =begin
 
@@ -33,9 +71,9 @@ puts pokedex.get_stats_name(randPoke)[2]
 
 
 puts "pick your pokemon!"
-choice1 = gets.chomp
+$choice1 = gets.chomp
 
-puts "you picked #{choice1}!" 
+puts "you picked #{$choice1}!" 
 
 puts "View stats?"
 puts "y/n?"
@@ -43,16 +81,16 @@ x = gets.chomp
 
 if x == "y"
 then
-puts pokedex.get_stats_name(choice1)   
+puts $pokedex.get_stats_name($choice1)   
 =begin 
 Pick a cell of data to use in code:
 
 add [2] to show specific stat
-pokedex.get_stats_name(choice1)[1] => name
-pokedex.get_stats_name(choice1)[2] => type I
-pokedex.get_stats_name(choice1)[3] => type II
-pokedex.get_stats_name(choice1)[4] => HP
-pokedex.get_stats_name(choice1)[5] => ATK  
+$pokedex.get_stats_name($choice1)[1] => name
+$pokedex.get_stats_name($choice1)[2] => type I
+$pokedex.get_stats_name($choice1)[3] => type II
+$pokedex.get_stats_name($choice1)[4] => HP
+$pokedex.get_stats_name($choice1)[5] => ATK  
 
 =end
 
